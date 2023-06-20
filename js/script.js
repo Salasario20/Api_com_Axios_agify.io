1dca1f3538b31a4b272fbbbe65354b40a931920c
Salasasario0 <salasario346827@gmail.com>
2023-06-19 21:33:07 -0300

    js pronto

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
 
 function pesquisarNome() {
     const nomeInput = document.querySelector("#api-input").value;
-      
+
     axios.get(`https://api.agify.io/?name=${nomeInput}`)
-      .then(function (response) {
-       const resultado = response.data;
-        console.log("idade: " + resultado['age']);
-        console.log( "nome: " + resultado['name' ]);
-        if(resultado['age'] === null){
-            console.log("TA entrando no if");
-            alert("Nome não encontrado");
-            
-          }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+        .then(function (response) {
+            const resultado = response.data;
+
+            if (resultado['age'] === null) {
+
+                alert("Nome não encontrado");
+                location.reload();
+
+            }
+            else {
+                console.log("idade: " + resultado['age']);
+                console.log("nome: " + resultado['name']);
+                const nameResultado = resultado['name'];
+                const ageResultado = resultado['age'];
+                const li1 = document.querySelector(".li-name");
+                const li2 = document.querySelector(".li-age");
+                
+                
+                li2.textContent = `Nome: ${nameResultado}`;
+                li1.textContent = `Suposta idade: ${ageResultado}`;
+                const ulElement = document.querySelector("#ul-api");
+                ulElement.appendChild(li2);
+                ulElement.appendChild(li1);
+            }
+        })
+        .catch(function (error) {
+            alert(error);
+            location.reload();
+        });
 
 
-     
-  }
-  
+
+}
+
 
 
 
